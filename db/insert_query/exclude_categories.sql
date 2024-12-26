106433e79dfd9dc4a4624f5a487793a94e10a1d9
tarr4h <81558152+tarr4h@users.noreply.github.com>
2024-12-27 00:27:05 +0900

    edit ignore

---
+++ db/insert_query/exclude_categories.sql
@@ -0,0 +1,15 @@
+INSERT INTO exclude_categories (category_name, related_mcid, update_dt) VALUES ('도로', 'DINING', '2024-07-21 11:15:53.453324');
+INSERT INTO exclude_categories (category_name, related_mcid, update_dt) VALUES ('시설', 'DINING', '2024-07-21 11:16:01.899288');
+INSERT INTO exclude_categories (category_name, related_mcid, update_dt) VALUES ('유통', 'CAFE', '2024-07-21 11:16:45.506223');
+INSERT INTO exclude_categories (category_name, related_mcid, update_dt) VALUES ('도로', 'ACCOMMODATION', '2024-08-04 08:47:49.970435');
+INSERT INTO exclude_categories (category_name, related_mcid, update_dt) VALUES ('도로', 'CAFE', '2024-08-04 15:00:59.974589');
+INSERT INTO exclude_categories (category_name, related_mcid, update_dt) VALUES ('시설', 'CAFE', '2024-08-04 15:01:07.939624');
+INSERT INTO exclude_categories (category_name, related_mcid, update_dt) VALUES ('시설', 'ACCOMMODATION', '2024-08-04 15:01:11.459765');
+INSERT INTO exclude_categories (category_name, related_mcid, update_dt) VALUES ('유통', 'DINING', '2024-08-04 15:01:55.249841');
+INSERT INTO exclude_categories (category_name, related_mcid, update_dt) VALUES ('유통', 'ACCOMMODATION', '2024-08-04 15:02:01.150871');
+INSERT INTO exclude_categories (category_name, related_mcid, update_dt) VALUES ('기업', 'CAFE', '2024-08-04 15:02:54.565918');
+INSERT INTO exclude_categories (category_name, related_mcid, update_dt) VALUES ('기업', 'DINING', '2024-08-04 15:02:59.077127');
+INSERT INTO exclude_categories (category_name, related_mcid, update_dt) VALUES ('기업', 'ACCOMMODATION', '2024-08-04 15:03:02.865995');
+INSERT INTO exclude_categories (category_name, related_mcid, update_dt) VALUES ('쇼핑', 'ACCOMMODATION', '2024-08-04 15:04:23.070273');
+INSERT INTO exclude_categories (category_name, related_mcid, update_dt) VALUES ('쇼핑', 'CAFE', '2024-08-04 15:04:25.134523');
+INSERT INTO exclude_categories (category_name, related_mcid, update_dt) VALUES ('쇼핑', 'DINING', '2024-08-04 15:04:32.578351');
---
+++ db/insert_query/vanish_map_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO vanish_map_data (id, request_cnt, init_dt, vanish_yn) VALUES ('987778513', 3, '2024-06-05 21:27:42.283425', 'y');
+INSERT INTO vanish_map_data (id, request_cnt, init_dt, vanish_yn) VALUES ('998736847', 4, '2024-06-06 16:19:00.644560', 'y');
+INSERT INTO vanish_map_data (id, request_cnt, init_dt, vanish_yn) VALUES ('998736453', 8, '2024-06-06 20:52:09.604899', 'y');
+INSERT INTO vanish_map_data (id, request_cnt, init_dt, vanish_yn) VALUES ('982913273', 3, '2024-06-07 02:56:59.058679', 'y');
+INSERT INTO vanish_map_data (id, request_cnt, init_dt, vanish_yn) VALUES ('981619173', 2, '2024-07-24 13:47:29.201357', 'y');
+INSERT INTO vanish_map_data (id, request_cnt, init_dt, vanish_yn) VALUES ('981618688', 4, '2024-07-24 13:47:44.798349', 'y');
+INSERT INTO vanish_map_data (id, request_cnt, init_dt, vanish_yn) VALUES ('984203503', 2, '2024-09-29 13:12:01.406142', 'y');
+INSERT INTO vanish_map_data (id, request_cnt, init_dt, vanish_yn) VALUES ('998732980', 2, '2024-12-10 14:45:28.681535', 'y');
